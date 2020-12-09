6708a1877bad6cbb0277b6967b0b494e8f029fb0
mbty <mbty@protonmail.com>
2020-12-09 19:40:51 +0100

    MSetWeakList -> list

--- examples/rv2/ISA.v
+++ examples/rv2/ISA.v
@@ -1,30 +1,9 @@
-Require Import MSetInterface MSetWeakList.
+Inductive memory_model  := RVWMO.
+Inductive base_standard := RV32I | RV64I.
+Inductive extension     := RVM   | RVA | RVF | RVD | RVQ | RVZiCSR | RVZifencei.
 
-(* Standards *)
-Inductive memory_model : Type :=
-| RVWMO.
-
-Inductive base_standard : Type :=
-| RV32I | RV64I.
-
-Inductive extension : Type :=
-| RVM | RVA | RVF | RVD | RVQ | RVZiCSR | RVZifencei.
-
-(* Extensions Set *)
-Scheme Equality for extension.
-
-Module DecidableExtension <: DecidableType.
-  Definition t := extension.
-  Definition eq := @eq extension.
-  Instance eq_equiv : @Equivalence extension eq := eq_equivalence.
-  Definition eq_dec := extension_eq_dec.
-End DecidableExtension.
-
-Module ExtensionsSet := MSetWeakList.Make DecidableExtension.
-
-(* ISA *)
-Record ISA : Type := {
-  ISA_memory_model: memory_model;
+Record ISA := {
+  ISA_memory_model  : memory_model;
   ISA_base_standard : base_standard;
-  ISA_activated_extensions : ExtensionsSet.t;
+  ISA_extensions    : list extension;
 }.
